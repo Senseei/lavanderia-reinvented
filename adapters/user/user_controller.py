6a84624d936c6d87fdfcbffa77a38ad544cf1844
senseei <gabrieldjesus475@gmail.com>
2025-05-19 02:38:08 -0300

    feat: refactor UserController to use AuthService for user login

--- adapters/user/user_controller.py
+++ adapters/user/user_controller.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request, render_template, session, redirect
 
 from application.errors.entity_not_found_error import EntityNotFoundError
+from application.user.use_cases.auth_service import AuthService
+
 
 class UserController:
-    def __init__(self, user_repository):
-        self.user_repository = user_repository
+    def __init__(self, auth_service: AuthService):
+        self.auth_service = auth_service
         self.user_blueprint = Blueprint("user", __name__)
 
         @self.user_blueprint.route("/login", methods=["GET", "POST"])
@@ -13,7 +15,7 @@ class UserController:
                 username = request.form.get("username")
                 password = request.form.get("password")
                 try:
-                    user = self.user_repository.find_by_username_and_password(username, password)
+                    user = self.auth_service.login(username, password)
                     session["user"] = user
                     return redirect("/")
                 except EntityNotFoundError:
