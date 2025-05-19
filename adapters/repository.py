896da19584236bc9afc3f8c29482ec6e666540ad
senseei <gabrieldjesus475@gmail.com>
2025-05-19 00:39:14 -0300

    feat: implement abstract Repository class for domain entity management

---
+++ adapters/repository.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+from domain.domain_entity import DomainEntity
+
+
+class Repository(ABC):
+
+    @abstractmethod
+    def save(self, entity: DomainEntity) -> DomainEntity:
+        """
+        Save a domain entity to the repository.
+        :param entity: The domain entity to save.
+        :return: The saved domain entity.
+        """
+        pass
+
+    @abstractmethod
+    def findById(self, entity_id: int) -> DomainEntity:
+        """
+        Find a domain entity by its ID.
+        :param entity_id: The ID of the domain entity to find.
+        :return: The domain entity with the specified ID, or None if not found.
+        """
+        pass
+
+    @abstractmethod
+    def findAll(self):
+        """
+        Find all domain entities in the repository.
+        :return: A list of all domain entities.
+        """
+        pass
+
+    @abstractmethod
+    def delete(self, entity: DomainEntity):
+        """
+        Delete a domain entity from the repository.
+        :param entity: The domain entity to delete.
+        """
+        pass
+
+    @abstractmethod
+    def deleteById(self, entity_id: int):
+        """
+        Delete a domain entity by its ID.
+        :param entity_id: The ID of the domain entity to delete.
+        """
+        pass
+
+    @abstractmethod
+    def update(self, entity: DomainEntity) -> DomainEntity:
+        """
+        Update a domain entity in the repository.
+        :param entity: The domain entity to update.
+        :return: The updated domain entity.
+        """
+        pass
