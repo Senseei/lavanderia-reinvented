2dbdbd5cded86595bda0d85920ff95b77b63e7a7
senseei <gabrieldjesus475@gmail.com>
2025-05-19 02:24:32 -0300

    fix: correct user ID assignment in AuthenticatedUserDTO constructor

--- application/user/dtos/authenticated_user_dto.py
+++ application/user/dtos/authenticated_user_dto.py
@@ -3,6 +3,6 @@ from domain.user import User
 
 class AuthenticatedUserDTO:
     def __init__(self, user: User):
-        self.id = id
+        self.id = user.id
         self.name = user.name
         self.cash = user.cash
