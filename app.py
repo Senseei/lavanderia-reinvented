f028ca4ebcc8edce67c590c84b7041c9753b916f
senseei <gabrieldjesus475@gmail.com>
2025-05-20 17:23:19 -0300

    feat: reorganize template and static file structure for improved maintainability and update resource references

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_session import Session
 from infrastructure.flask.routes.index_router import IndexRouter
 
 app = Flask(__name__,
-            template_folder='infrastructure/flask/templates',
-            static_folder='infrastructure/flask/static')
+           static_folder='infrastructure/flask/resources/static',
+           template_folder='infrastructure/flask/resources/templates')
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
