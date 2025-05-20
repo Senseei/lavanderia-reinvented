285800ad58c2deb9d342eaf15491ba9d0a09c9c7
senseei <gabrieldjesus475@gmail.com>
2025-05-19 23:27:12 -0300

    feat: extend DomainEntity constructor to include created_at and updated_at timestamps

--- domain/domain_entity.py
+++ domain/domain_entity.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from abc import ABC
 
 class DomainEntity(ABC):
-    def __init__(self, id: int):
+    def __init__(self, id: int, created_at: datetime = datetime.now(), updated_at: datetime = datetime.now()):
         self.id = id
+        self.created_at = created_at
+        self.updated_at = updated_at
