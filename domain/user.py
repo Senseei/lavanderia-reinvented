4b43be7ba008ddbeee1e89785a0daa01664c9625
senseei <gabrieldjesus475@gmail.com>
2025-05-19 23:27:04 -0300

    feat: update User class constructor to make id optional and set default cash value

--- domain/user.py
+++ domain/user.py
@@ -2,7 +2,7 @@ from domain_entity import DomainEntity
 
 
 class User(DomainEntity):
-    def __init__(self, id: int, username: str, name: str, password: str, cash: float):
+    def __init__(self, username: str, name: str, password: str, cash: float = 0, id: int = None):
         super().__init__(id)
         self.name = name
         self.username = username
