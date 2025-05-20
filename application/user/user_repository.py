b6428c2c36565acf811b38aff3d257c6c0ed9842
senseei <gabrieldjesus475@gmail.com>
2025-05-19 23:08:05 -0300

    feat: refactor Repository and UserRepository to use generics for improved type safety

--- application/user/user_repository.py
+++ application/user/user_repository.py
@@ -1,20 +1,30 @@
 from abc import ABC, abstractmethod
 
 from adapters.repository import Repository
+from domain.user import User
 
 
-class UserRepository(Repository, ABC):
+class UserRepository(Repository[User], ABC):
     """
     UserRepository is an abstract base class that defines the interface for user-related data access operations.
     It extends the Repository class, which provides a generic interface for data access.
     """
 
     @abstractmethod
-    def find_by_username_and_password(self, username: str, password: str):
+    def find_by_username_and_password(self, username: str, password: str) -> User:
         """
         Find a user by username and password.
         :param username: The username of the user to find.
         :param password: The password of the user to find.
         :return: The user with the specified username and password, or None if not found.
+        """
+        pass
+
+    @abstractmethod
+    def find_by_username(self, username: str) -> User:
+        """
+        Find a user by username.
+        :param username: The username of the user to find.
+        :return: The user with the specified username, or None if not found.
         """
         pass
