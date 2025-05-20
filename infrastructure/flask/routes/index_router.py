a4517a9776c69f487b4561bc174a899c8ed4c6b6
senseei <gabrieldjesus475@gmail.com>
2025-05-20 00:24:13 -0300

    feat: add UnitControllerAdapter and UnitService integration in IndexRouter and AuthRouter

--- infrastructure/flask/routes/index_router.py
+++ infrastructure/flask/routes/index_router.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, render_template, session, redirect
 
+from adapters.unit.unit_controller import UnitControllerAdapter
+from application.unit.usecases.unit_service import UnitService
+from infrastructure.db.sqlite3.repositories.unit_repository import UnitRepositoryImpl
 from infrastructure.flask.routes.auth.auth_router import AuthRouter
 from infrastructure.flask.routes.base_router import BaseRouter
 
 
 class IndexRouter(BaseRouter):
+    _unit_controller: UnitControllerAdapter
+
     def __init__(self):
         super().__init__(Blueprint("index", __name__, url_prefix="/"))
+        self.resolve_dependencies()
 
         self.register_routes([
             AuthRouter().blueprint,
@@ -17,9 +23,9 @@ class IndexRouter(BaseRouter):
             if not session.get("user_id"):
                 return redirect("/login")
 
-            # TODO: Implement the logic to fetch locations from the database
-            return render_template("index.html", locations={})
+            return render_template("index.html", locations=self._unit_controller.find_all())
 
-    @classmethod
-    def resolve_dependencies(cls):
-        pass
+    def resolve_dependencies(self):
+        unit_repository = UnitRepositoryImpl()
+        unit_service = UnitService(unit_repository)
+        self._unit_controller = UnitControllerAdapter(unit_service)
