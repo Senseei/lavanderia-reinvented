fb7a6d05899cfd3325df2668b706d531fe74e470
senseei <gabrieldjesus475@gmail.com>
2025-05-20 23:43:51 -0300

    feat: implement UnitRepositoryImpl with CRUD operations and add find_by_local method

--- infrastructure/db/sqlite3/repositories/unit_repository.py
+++ infrastructure/db/sqlite3/repositories/unit_repository.py
@@ -1,26 +1,62 @@
+from typing import Optional
+
 from application.unit.interfaces.unit_repository import UnitRepository
 from domain.unit import Unit
+from infrastructure.db.sqlite3.repositories.sqlite_repository import SqliteRepository
 
 
-class UnitRepositoryImpl(UnitRepository):
+class UnitRepositoryImpl(UnitRepository, SqliteRepository):
 
     def save(self, entity: Unit) -> Unit:
-        pass
+        cursor = self.get_cursor()
+        if not entity.id:
+            cursor.execute("INSERT INTO units (local) VALUES (?)", entity.local)
+            entity.id = cursor.lastrowid
+        else:
+            cursor.execute(
+                "UPDATE units SET local = ? WHERE id = ?",
+                (entity.local, entity.id)
+            )
+        self.commit()
+        return entity
 
-    def find_by_id(self, entity_id: int) -> Unit:
-        pass
+    def find_by_id(self, entity_id: int) -> Optional[Unit]:
+        cursor = self.get_cursor()
+        cursor.execute("SELECT * FROM units WHERE id = ?", (entity_id,))
+        row = cursor.fetchone()
+        if row:
+            return Unit(
+                id=row['id'],
+                local=row['local']
+            )
+        return None
 
     def find_all(self) -> list[Unit]:
-        pass
+        cursor = self.get_cursor()
+        cursor.execute("SELECT * FROM units")
+        rows = cursor.fetchall()
+        return [
+            Unit(
+                id=row['id'],
+                local=row['local']
+            ) for row in rows
+        ]
 
     def delete(self, entity: Unit):
-        pass
+        self.delete_by_id(entity.id)
 
     def delete_by_id(self, entity_id: int):
-        pass
+        cursor = self.get_cursor()
+        cursor.execute("DELETE FROM units WHERE id = ?", (entity_id,))
+        self.commit()
 
-    def update(self, entity: Unit) -> Unit:
-        pass
-
-    def find_by_name(self, name: str) -> Unit:
-        pass
+    def find_by_local(self, name: str) -> Optional[Unit]:
+        cursor = self.get_cursor()
+        cursor.execute("SELECT * FROM units WHERE local = ?", (name,))
+        row = cursor.fetchone()
+        if row:
+            return Unit(
+                id=row['id'],
+                local=row['local']
+            )
+        return None
