4bfba12a2ea908823793888a217bd87e61326a5a
senseei <gabrieldjesus475@gmail.com>
2025-05-20 23:47:34 -0300

    feat: update UnitDTO to use 'local' attribute instead of 'name'

--- application/unit/dtos/unit_dto.py
+++ application/unit/dtos/unit_dto.py
@@ -4,6 +4,6 @@ from domain.unit import Unit
 class UnitDTO:
     def __init__(self, unit: Unit):
         self.id = unit.id
-        self.name = unit.name
+        self.local = unit.local
         self.created_at = unit.created_at
         self.updated_at = unit.updated_at
