7c50a2878ca497a4c503d97eeee07e6afe545345
senseei <gabrieldjesus475@gmail.com>
2025-05-21 00:19:13 -0300

    feat: update unit retrieval methods to return ResponseDTO and handle entity not found errors

--- application/unit/usecases/unit_service.py
+++ application/unit/usecases/unit_service.py
@@ -1,5 +1,7 @@
+from application.errors.entity_not_found_error import EntityNotFoundError
 from application.unit.dtos.unit_dto import UnitDTO
 from application.unit.interfaces.unit_repository import UnitRepository
+from domain.unit import Unit
 
 
 class UnitService:
@@ -8,3 +10,9 @@ class UnitService:
 
     def find_all(self) -> list[UnitDTO]:
         return list(map(UnitDTO, self._repository.find_all()))
+
+    def find_by_id(self, entity_id: int) -> UnitDTO:
+        unit = self._repository.find_by_id(entity_id)
+        if unit is None:
+            raise EntityNotFoundError(Unit.__class__.__name__, entity_id)
+        return UnitDTO(unit)
