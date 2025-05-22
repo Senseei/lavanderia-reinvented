e7c1fecb678ba3ef907f86e3baa9d3f388e938e6
senseei <gabrieldjesus475@gmail.com>
2025-05-22 00:20:34 -0300

    feat: implement thread-local database connection management

--- infrastructure/db/sqlite3/database_config.py
+++ infrastructure/db/sqlite3/database_config.py
@@ -1,31 +1,30 @@
 import sqlite3
 from typing import Optional
 from threading import Lock
+import threading
 
 
 class DatabaseConfig:
     _instance: Optional['DatabaseConfig'] = None
     _lock: Lock = Lock()
-    _connection: Optional[sqlite3.Connection] = None
+    _local = threading.local()
 
     def __new__(cls):
         with cls._lock:
             if cls._instance is None:
                 cls._instance = super(DatabaseConfig, cls).__new__(cls)
-                cls._instance._connection = None
-                cls._instance._db_file = 'database.db' # TODO alterar para usar do .env
+                cls._instance._db_file = 'database.db'  # TODO alterar para usar do .env
             return cls._instance
 
     def get_connection(self) -> sqlite3.Connection:
-        """Obtém a conexão com o banco de dados"""
-        if self._connection is None:
-            self._connection = sqlite3.connect(self._db_file)
-            # Para retornar resultados como dicionários
-            self._connection.row_factory = sqlite3.Row
-        return self._connection
+        """Obtém a conexão com o banco de dados específica para cada thread"""
+        if not hasattr(self._local, 'connection'):
+            self._local.connection = sqlite3.connect(self._db_file)
+            self._local.connection.row_factory = sqlite3.Row
+        return self._local.connection
 
     def close(self) -> None:
-        """Fecha a conexão com o banco de dados"""
-        if self._connection:
-            self._connection.close()
-            self._connection = None
+        """Fecha a conexão com o banco de dados para a thread atual"""
+        if hasattr(self._local, 'connection'):
+            self._local.connection.close()
+            delattr(self._local, 'connection')
