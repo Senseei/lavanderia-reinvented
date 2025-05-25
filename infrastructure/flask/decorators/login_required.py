63399f54558abbec3e973ff4c65871d90512a154
senseei <gabrieldjesus475@gmail.com>
2025-05-25 13:41:19 -0300

    feat: update login_required to use url_for for login path redirection

--- infrastructure/flask/decorators/login_required.py
+++ infrastructure/flask/decorators/login_required.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from flask import session, render_template
+from flask import session, render_template, url_for
 
 from infrastructure.flask.routes.auth.routes_constants import AuthRoutes
 
@@ -12,6 +12,6 @@ def login_required(f):
             return render_template(
                 "alert.html",
                 message="It seems you are not logged in or your session has expired. Please log in again.",
-                path=AuthRoutes.LOGIN_FULL_PATH)
+                path=url_for('index.auth.login'))
         return f(*args, **kwargs)
     return decorated_function
