5ffc04bc31652685737821f728c5e1c869a94537
senseei <gabrieldjesus475@gmail.com>
2025-05-25 12:51:02 -0300

    feat: update Card class constructor to make id optional and add final method for masked number display

--- domain/card.py
+++ domain/card.py
@@ -7,11 +7,14 @@ from user import User
 
 
 class Card(DomainEntity):
-    def __init__(self, id: int, user: User, brand: CardBrand, number: str, method: PaymentMethod, due_date: datetime, cv: str):
+    def __init__(self, user: User, brand: CardBrand, number: str, method: PaymentMethod, due_date: datetime, cv: str, id: int = None):
         super().__init__(id)
         self.user = user
         self.brand = brand
-        self.humber = number
+        self.number = number
         self.method = method
         self.due_date = due_date
         self.cv = cv
+
+    def final(self, mask: bool = True) -> str:
+        return f"**** **** **** {self.number[-4:]}" if mask else self.number[-4:]
