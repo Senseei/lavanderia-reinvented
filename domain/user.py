6dca4397eea6e1c8b888e56bc3a475ff8a820d5e
senseei <gabrieldjesus475@gmail.com>
2025-05-26 19:20:21 -0300

    feat: update machine identifier display in cart and set default cash for new users

--- domain/user.py
+++ domain/user.py
@@ -2,7 +2,7 @@ from domain.domain_entity import DomainEntity
 
 
 class User(DomainEntity):
-    def __init__(self, username: str, name: str, password: str, cash: float = 0, id: int = None):
+    def __init__(self, username: str, name: str, password: str, cash: float = 2000, id: int = None):
         super().__init__(id)
         self.name = name
         self.username = username
